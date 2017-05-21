cf94521b5c46de7570cbc7a8b1626e233a0ecbd5
Vlad Hartmann <hartmannvlad@gmail.com>
2017-05-21 20:57:12 +0100

    Added UI for recipes and shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "Test description", "http://i.dailymail.co.uk/i/pix/2016/07/25/18/3695AB9400000578-3707201-Importantly_the_damage_sperm_by_fatty_food_is_not_necessarily_pe-a-9_1469468033481.jpg")
+  ];
 
   constructor() { }
 
